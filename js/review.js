795fc5991e46e76bad0edd794866b000a49087b5
edwinv123 <126evalentinnunez@frhsd.com>
2024-10-02 12:52:18 -0400

    first commit

---
+++ js/review.js
@@ -0,0 +1,29 @@
+console.log("This is external");
+console.log("Work on typing skills");
+// Variable
+let variableName;
+const myName = 'Edwin';
+
+let lukeAge = 16;
+let tylerAge= 17;
+let kevinAge= 18;
+let mcdonaldAge= 33;
+
+
+
+
+ let old = lukeAge + mcdonaldAge;
+console.log("Old Age = " + old);
+console.log("old");
+console.log(old + kevinAge);
+// old + old KevinAge
+old + kevinAge;
+console.log(old);
+
+
+
+// Aritmetic
+console.log(10 % 2);
+console.log(14 % 2);
+console.log(35 % 2);
+
